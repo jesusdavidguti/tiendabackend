d93ed56a085e951395b84cdc7752acb244b4fb00
Tu Nombre <you@example.com>
2021-01-15 13:03:04 +0100

    Conexión BB.DD.

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
+require('dotenv').config(); // --> lee las variables de entorno del archivo .env y las
+                            // incorpora a process.env.PORT
 const path       = require('path');
 const cors       = require('cors');
 const express    = require('express');
-const mongoose   = require('mongoose');
-const apiRoutes  = require('./routes');
+const mongoose   = require('mongoose'); // Es un paquete
+const apiRoutes  = require('./routes'); // Es un archivo
 
 const app    = express();
-const PORT   = process.env.PORT || 3000;
+const PORT   = process.env.PORT || 3000; // "process" es un objeto que contiene información acerca
+                                         // de la aplicación. No es necesario importarlo, pero se
+                                         // podría. Contiene muchas propiedades y métodos.
 const DB_URI = process.env.DB_URI;
-
 
 // CONEXIÓN A BASE DE DATOS
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
